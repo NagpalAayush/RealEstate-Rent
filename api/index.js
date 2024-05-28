9a37387c4faec85d133d4e6d399512e4cd00bc21
Aayush <aayushnagpal90@gmail.com>
2024-05-29 00:21:53 +0530

    Connected to Database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
 const app = express();
+
+dotenv.config()
+
+
+// Connecting to DataBase
+async function main() {
+  await mongoose.connect(process.env.MONGO);
+}
+
+main()
+  .then(() => {
+    console.log("Connected to Database Successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, (req, res) => {
   console.log("server listning on port 3000");
