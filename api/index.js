544c6e8b51f089f95fd4f6380012f58111895ac6
Aayush <aayushnagpal90@gmail.com>
2024-05-29 21:02:27 +0530

    Created a test API route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv"
 const app = express();
+import userRouter from "./routes/user.Routes.js"
 
 dotenv.config()
 
@@ -18,6 +19,9 @@ main()
   .catch((err) => {
     console.log(err);
   });
+
+
+ app.use("/" , userRouter);
 
 app.listen(3000, (req, res) => {
   console.log("server listning on port 3000");
