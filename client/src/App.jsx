64957d3b844a97bfb552486f143b3e6b28da20b9
Aayush <aayushnagpal90@gmail.com>
2024-06-04 14:12:30 +0530

    Corrected path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn.jsx';
+import SignUp from './pages/SignUp.jsx';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
